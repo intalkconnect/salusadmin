e48e8d407a222efd74fc90cf50d0b2b982fcbc98
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-20 13:35:02 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // 👈 Isso evita que o Vercel quebre o build por erros de lint
+  },
 };
 
 export default nextConfig;
