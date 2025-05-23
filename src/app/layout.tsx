b731d73cd87a71f5bc2331c78202b5e79debd338
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-23 18:45:59 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt">
+    <html lang="pt" className="dark">>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
