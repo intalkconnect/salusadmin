539f4e0e81cfbf38d08f1eeb7cef4c1cd0840ae8
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-24 19:53:37 -0300

    Update InstanceCard.tsx

--- src/components/InstanceCard.tsx
+++ src/components/InstanceCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { Cliente } from "../app/lib/types";
+import { Cliente } from "@/app/lib/types";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Switch } from "@/components/ui/switch";
